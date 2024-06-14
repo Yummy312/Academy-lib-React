329295ac5be769afcbdaf0236d05a9dfb5dd7e40
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2024-06-14 20:30:51 +0300

    fix config v3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Academy-lib-React/",
+  base: "/Academy-lib-React",
   plugins: [react()],
 })
